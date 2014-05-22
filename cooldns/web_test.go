f1050d0d9b4d4c07ed878ec5e44ac223c542d507
Matti <matti@moell.org>
2014-05-22 20:09:37 +0200

    web: Added missing testcases
    We now test the whole internet API. yay, only Err500s are missing.

--- cooldns/web_test.go
+++ cooldns/web_test.go
@@ -2,11 +2,13 @@ package cooldns
 
 import (
 	"bytes"
+	"code.google.com/p/go.net/html"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -53,6 +55,21 @@ func TestGetIndex(t *testing.T) {
 	}
 }
 
+func TestGetUpdate(t *testing.T) {
+	server := createTestServer(t)
+	defer server.S.Close()
+
+	resp, err := http.Get(server.S.URL + "/update")
+	if err != nil {
+		t.Log(server.Log.String())
+		t.Error("Failed to connect to server")
+	}
+	if resp.StatusCode != 200 {
+		t.Log(server.Log.String())
+		t.Error("Status for \"/\" is not 200")
+	}
+}
+
 type updateTest struct {
 	Domain   string
 	Password string
@@ -64,7 +81,7 @@ type updateTest struct {
 
 var updatetests = []updateTest{
 	updateTest{
-		"hallo",
+		"hallo.ist.nicht.cool.",
 		"123456789",
 		"192.168.0.1",
 		"Hallo Welt",
@@ -72,7 +89,7 @@ var updatetests = []updateTest{
 		false,
 	},
 	updateTest{
-		"tadaaa",
+		"tadaaa.ist.nicht.cool.",
 		"blablablabla",
 		"1.1.1.1",
 		"Blab Bla srdgojfg kfjghn",
@@ -80,7 +97,7 @@ var updatetests = []updateTest{
 		false,
 	},
 	updateTest{
-		"lalala",
+		"lalala.ist.nicht.cool.",
 		"sdgkasgkomasfdkog",
 		"6.6.6.6",
 		"sgasg awgdasd asdfg dfasg sdfgdf",
@@ -88,7 +105,7 @@ var updatetests = []updateTest{
 		false,
 	},
 	updateTest{
-		"lalalaergsdfkln",
+		"lalalaergsdfkln.ist.nicht.cool.",
 		"sdgkasgkomasfdkog",
 		"6.6.6.256",
 		"sgasg awgdasd asdfg dfasg sdfgdf",
@@ -121,7 +138,7 @@ func TestUpdateDynApi(t *testing.T) {
 	server := createTestServer(t)
 	defer server.S.Close()
 	for _, test := range updatetests {
-		auth, err := NewAuth(test.Domain+".ist.nicht.cool.", test.Password)
+		auth, err := NewAuth(test.Domain, test.Password)
 		if err != nil {
 			t.Fatal("Creating new user failed")
 		}
@@ -130,7 +147,7 @@ func TestUpdateDynApi(t *testing.T) {
 		}
 
 		// Setup URL
-		domain := test.Domain + ".ist.nicht.cool."
+		domain := test.Domain
 		v := url.Values{}
 		v.Set("hostname", domain)
 		v.Set("myip", test.Ip)
@@ -196,19 +213,22 @@ var updateerrorfieldtests = []updateErrorFieldsTest{
 	updateErrorFieldsTest{"", "192.168.0.1", "Hallo Welt", 401},
 	// Not an IP
 	updateErrorFieldsTest{"testtest.ist.nicht.cool.", "192.168.0.bla1", "", 400},
+	updateErrorFieldsTest{"testtest.ist.nicht.cool.noexist", "192.168.0.1", "", 401},
 }
 
 type updateErrorAuthTest struct {
 	Fields *updateErrorFieldsTest
-	Pass   string
 	User   string
+	Pass   string
 }
 
 var updatecorrectfields = updateErrorFieldsTest{"testtest.ist.nicht.cool.", "192.168.0.1", "Hallo Welt", 401}
 
+var updatenotexistfields = updateErrorFieldsTest{"testtest.ist.nicht.cool.not.exist.", "192.168.0.1", "Hallo Welt", 401}
+
 var updateerrorauthtests = []updateErrorAuthTest{
 	// No User name
-	updateErrorAuthTest{&updatecorrectfields, ".", "123456789"},
+	updateErrorAuthTest{&updatecorrectfields, "", "123456789"},
 	// No password
 	updateErrorAuthTest{&updatecorrectfields, "testtest.ist.nicht.cool.", ""},
 	// Neither password nor user name
@@ -216,7 +236,7 @@ var updateerrorauthtests = []updateErrorAuthTest{
 	// Wrong Username
 	updateErrorAuthTest{&updatecorrectfields, "testtest.jsagjcool.", "123456789"},
 	// Wrong Password
-	updateErrorAuthTest{&updatecorrectfields, "testtest.ist.nicht.cool.", "wrong wrong"},
+	updateErrorAuthTest{&updatecorrectfields, "testtest.ist.nicht.cool.", "wrongwrong"},
 }
 
 func TestUpdateDynApiError(t *testing.T) {
@@ -236,9 +256,15 @@ func TestUpdateDynApiError(t *testing.T) {
 	// Check for malformatted fields
 	for _, test := range updateerrorfieldtests {
 		v := url.Values{}
-		v.Set("hostname", test.Domain)
-		v.Set("myip", test.Ip)
-		v.Set("txt", test.Txt)
+		if test.Domain != "" {
+			v.Set("hostname", test.Domain)
+		}
+		if test.Ip != "" {
+			v.Set("myip", test.Ip)
+		}
+		if test.Txt != "" {
+			v.Set("txt", test.Txt)
+		}
 		URL := getUpdateURL(domain, password, server.S.URL, v)
 
 		resp, err := http.Get(URL.String())
@@ -276,4 +302,456 @@ func TestUpdateDynApiError(t *testing.T) {
 		}
 	}
 
+	// check for no auth
+	v := url.Values{}
+	v.Set("hostname", domain)
+	v.Set("myip", "1.1.1.1")
+	v.Set("txt", "Hallololololol")
+	URL := getUpdateURL(domain, password, server.S.URL, v)
+	URL.User = nil
+
+	resp, err := http.Get(URL.String())
+	if err != nil {
+		t.Log(server.Log.String())
+		t.Fatal("Failed to update URL:", URL.String(), err)
+	}
+	if resp.StatusCode != 401 {
+		t.Log(server.Log.String())
+		t.Errorf("AuthCheck: Wrong return Code: Got %d, expected 401. \n\tTest: %v",
+			resp.StatusCode,
+			"Check for no auth")
+	}
+
+	// check totaly not existing entry
+	v = url.Values{}
+	v.Set("hostname", updatenotexistfields.Domain)
+	v.Set("myip", updatenotexistfields.Ip)
+	v.Set("txt", updatenotexistfields.Txt)
+	URL = getUpdateURL(updatenotexistfields.Domain, password, server.S.URL, v)
+	resp, err = http.Get(URL.String())
+	if err != nil {
+		t.Log(server.Log.String())
+		t.Fatal("Failed to update URL:", URL.String(), err)
+	}
+	if resp.StatusCode != 401 {
+		t.Log(server.Log.String())
+		t.Errorf("AuthCheck: Wrong return Code: Got %d, expected 401. \n\tTest: %v",
+			resp.StatusCode,
+			"Check for not existing auth")
+	}
+}
+
+func getFormNewURL(server string) *url.URL {
+	URL, err := url.Parse(server)
+	if err != nil {
+		return nil
+	}
+	URL.Path = "/"
+
+	return URL
+}
+
+type formdomainnewtest struct {
+	Domain   string
+	Secret   string
+	ErrCount int
+}
+
+var formdomainnewtests = []formdomainnewtest{
+	formdomainnewtest{"test1.ist.nicht.cool", "GeheimGeheim", 0},
+	formdomainnewtest{"withdot.ist.nicht.cool.", "GeheimGeheim", 0},
+	formdomainnewtest{"test2.ist.nicht.cool", "toshort", 1},
+	formdomainnewtest{"", "", 2},
+	formdomainnewtest{"1.ist.nicht.cool", "", 2},
+	formdomainnewtest{"", "strangeöäü?", 1},
+	formdomainnewtest{"2.ist.nicht.cool", "123", 1},
+	formdomainnewtest{"3.ist.nicht.cool", "strangeöäü?", 1},
+	formdomainnewtest{"exists.ist.nicht.cool", "blablablabla", 1},
+}
+
+func TestFormDomainNew(t *testing.T) {
+	server := createTestServer(t)
+	defer server.S.Close()
+	// Add Exists entry
+	domain := "exists.ist.nicht.cool."
+	secret := "supersecretsecret"
+	auth, _ := NewAuth(domain, secret)
+	server.Db.SaveAuth(auth)
+	server.Db.SaveEntry(&Entry{
+		Hostname: domain,
+		Offline:  false,
+	})
+
+	for _, test := range formdomainnewtests {
+		v := url.Values{}
+		if test.Domain != "" {
+			v.Set("domain", test.Domain)
+		}
+		if test.Secret != "" {
+			v.Set("secret", test.Secret)
+		}
+		URL := getFormNewURL(server.S.URL)
+
+		resp, err := http.PostForm(URL.String(), v)
+		if err != nil {
+			t.Log(server.Log.String())
+			t.Fatal("Failed to update URL:", URL.String(), err)
+		}
+		if resp.StatusCode != 200 {
+			t.Log(server.Log.String())
+			t.Errorf("FormNew: Wrong return Code: Got %d, expected %d. \n\tTest: %v",
+				resp.StatusCode,
+				200,
+				test)
+		}
+		doc, err := html.Parse(resp.Body)
+		if err != nil {
+			t.Fatal("Error parsing response Bopy")
+		}
+		errMsgs := checkForAlerts(doc)
+		if test.ErrCount != len(errMsgs) {
+			t.Errorf("Should have %d alert-warnings found: %d \nErrors: &v", test.ErrCount, len(errMsgs), errMsgs)
+		}
+
+		// Post Db check if user was actually created
+		if test.ErrCount == 0 {
+			// Check auth
+			var d string
+			if strings.HasSuffix(test.Domain, ".") {
+				d = test.Domain
+			} else {
+				d = test.Domain + "."
+			}
+			a := server.Db.GetAuth(d)
+			if a == nil {
+				t.Error("User was not created:", d)
+				continue
+			}
+			ok, err := a.CheckAuth(d, test.Secret)
+			if err != nil {
+				t.Fatal("Failed to check authenticity")
+			}
+			if !ok {
+				t.Error("Saved Auth does not match input", test, a)
+			}
+
+			// Check entry
+			e := server.Db.GetEntry(d)
+			if e == nil || e.Hostname != d {
+				t.Error("Domain does not match entry", test, e)
+			}
+		}
+		if t.Failed() {
+			t.Logf("TEST: %v\n", test)
+			t.Log("\tErrMsgs:", errMsgs)
+			t.Log("\t", resp)
+			t.Log(server.Log.String())
+		}
+	}
+}
+
+// This is where it gets funny. We implement a little html parser to see how
+// many alert-warnings there are. Returns a String containing all error
+// messages.
+func checkForAlerts(doc *html.Node) []string {
+	var errors []string
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "div" {
+			for _, a := range n.Attr {
+				if a.Key == "class" {
+					if strings.Contains(a.Val, "alert-warning") {
+						errors = append(errors, n.FirstChild.Data)
+					}
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	return errors
+}
+
+func getFormUpdateURL(server string) *url.URL {
+	URL, err := url.Parse(server)
+	if err != nil {
+		return nil
+	}
+	URL.Path = "/update"
+
+	return URL
+}
+
+type formdomainupdatetest struct {
+	Domain   string
+	Secret   string
+	Ips      string
+	Cname    string
+	Mxs      string
+	Txts     string
+	ErrCount int
+	ExSecret string
+	Entry    Entry
+}
+
+var formdomainupdatetests = []formdomainupdatetest{
+	// Simple example
+	formdomainupdatetest{
+		Domain:   "test1.ist.nicht.cool",
+		Secret:   "123456789",
+		Ips:      "192.168.0.1\n 2a00:1450:4008:c01::65",
+		Cname:    "",
+		Mxs:      "10 mail.provider.tld",
+		Txts:     "",
+		ErrCount: 0,
+		ExSecret: "123456789",
+		Entry: Entry{
+			Hostname: "test1.ist.nicht.cool.",
+			Ip6s: []net.IP{
+				net.ParseIP("2a00:1450:4008:c01::65"),
+			},
+			Ip4s: []net.IP{
+				net.ParseIP("192.168.0.1"),
+			},
+			Offline: false,
+			Txts:    nil,
+			Mxs: []MxEntry{
+				MxEntry{
+					ip:       "mail.provider.tld.",
+					priority: 10,
+				},
+			},
+		},
+	},
+	// More complicated example
+	formdomainupdatetest{
+		Domain:   "test2.ist.nicht.cool",
+		Secret:   "123456789",
+		Ips:      "192.168.0.1 \n192.168.0.2 \n192.168.0.3 \n2a00:1450:4008:c01::64 \n192.168.0.4 \n2a00:1450:4008:c01::65",
+		Cname:    "",
+		Mxs:      "10 mail.provider.tld \n100 mail2.provider.tld",
+		Txts:     "",
+		ErrCount: 0,
+		ExSecret: "123456789",
+		Entry: Entry{
+			Hostname: "test2.ist.nicht.cool.",
+			Ip6s: []net.IP{
+				net.ParseIP("2a00:1450:4008:c01::64"),
+				net.ParseIP("2a00:1450:4008:c01::65"),
+			},
+			Ip4s: []net.IP{
+				net.ParseIP("192.168.0.1"),
+				net.ParseIP("192.168.0.2"),
+				net.ParseIP("192.168.0.3"),
+				net.ParseIP("192.168.0.4"),
+			},
+			Offline: false,
+			Txts:    []string{},
+			Mxs: []MxEntry{
+				MxEntry{
+					ip:       "mail.provider.tld.",
+					priority: 10,
+				},
+				MxEntry{
+					ip:       "mail2.provider.tld.",
+					priority: 100,
+				},
+			},
+			Cname: "",
+		},
+	},
+	// Empty example
+	formdomainupdatetest{
+		Domain:   "",
+		Secret:   "",
+		Ips:      "",
+		Cname:    "",
+		Mxs:      "",
+		Txts:     "",
+		ErrCount: 2,
+		Entry:    Entry{},
+	},
+	// Wrong password
+	formdomainupdatetest{
+		Domain:   "wrongpw.ist.nicht.cool",
+		Secret:   "wrong",
+		Ips:      "192.168.0.1\n 2a00:1450:4008:c01::65",
+		Cname:    "",
+		Mxs:      "10 mail.provider.tld",
+		Txts:     "",
+		ErrCount: 1,
+		ExSecret: "123456789",
+		Entry: Entry{
+			Hostname: "wrongpw.ist.nicht.cool.",
+			Ip6s: []net.IP{
+				net.ParseIP("2a00:1450:4008:c01::65"),
+			},
+			Ip4s: []net.IP{
+				net.ParseIP("192.168.0.1"),
+			},
+			Offline: false,
+			Txts:    nil,
+			Mxs: []MxEntry{
+				MxEntry{
+					ip:       "mail.provider.tld.",
+					priority: 10,
+				},
+			},
+		},
+	},
+	// non existing Domain
+	formdomainupdatetest{
+		Domain:   "noexist.ist.nicht.cool",
+		Secret:   "wrong",
+		Ips:      "192.168.0.1\n 2a00:1450:4008:c01::65",
+		Cname:    "",
+		Mxs:      "10 mail.provider.tld",
+		Txts:     "",
+		ErrCount: 1,
+		ExSecret: "123456789",
+		Entry:    Entry{},
+	},
+	// Errornous Ips but with fqdn
+	formdomainupdatetest{
+		Domain:   "sillyip.ist.nicht.cool.",
+		Secret:   "123456789",
+		Ips:      "192.168.300.1\n 2a00:1450:40x8:c01::65",
+		Cname:    "",
+		Mxs:      "10 mail.provider.tld",
+		Txts:     "",
+		ErrCount: 1,
+		ExSecret: "123456789",
+		Entry: Entry{
+			Hostname: "sillyip.ist.nicht.cool.",
+			Ip6s:     []net.IP{},
+			Ip4s:     []net.IP{},
+			Offline:  false,
+			Txts:     nil,
+			Mxs: []MxEntry{
+				MxEntry{
+					ip:       "mail.provider.tld.",
+					priority: 10,
+				},
+			},
+		},
+	},
+	// Errornous MX but with fqdn
+	formdomainupdatetest{
+		Domain:   "sillymx.ist.nicht.cool.",
+		Secret:   "123456789",
+		Ips:      "192.168.0.1\n 2a00:1450:4008:c01::65",
+		Cname:    "",
+		Mxs:      "1a mail.provider.tld",
+		Txts:     "",
+		ErrCount: 1,
+		ExSecret: "123456789",
+		Entry: Entry{
+			Hostname: "sillymx.ist.nicht.cool.",
+			Ip6s: []net.IP{
+				net.ParseIP("2a00:1450:4008:c01::65"),
+			},
+			Ip4s: []net.IP{
+				net.ParseIP("192.168.0.1"),
+			},
+			Offline: false,
+			Txts:    nil,
+			Mxs: []MxEntry{
+				MxEntry{
+					ip:       "mail.provider.tld.",
+					priority: 10,
+				},
+			},
+		},
+	},
+}
+
+func TestFormDomainUpdate(t *testing.T) {
+	server := createTestServer(t)
+	defer server.S.Close()
+
+	// Add entries based on entry element
+	for _, test := range formdomainupdatetests {
+		if test.Entry.Hostname == "" {
+			continue
+		}
+		auth, _ := NewAuth(test.Entry.Hostname, test.ExSecret)
+		err := server.Db.SaveAuth(auth)
+		if err != nil {
+			t.Fatal("Failed to create Users")
+		}
+		err = server.Db.SaveEntry(&Entry{
+			Hostname: test.Entry.Hostname,
+			Offline:  false,
+		})
+		if err != nil {
+			t.Fatal("Failed to save Entry")
+		}
+	}
+
+	for _, test := range formdomainupdatetests {
+		v := url.Values{}
+		if test.Domain != "" {
+			v.Set("domain", test.Domain)
+		}
+		if test.Secret != "" {
+			v.Set("secret", test.Secret)
+		}
+		if test.Cname != "" {
+			v.Set("cname", test.Cname)
+		}
+		if test.Ips != "" {
+			v.Set("ip", test.Ips)
+		}
+		if test.Mxs != "" {
+			v.Set("mx", test.Mxs)
+		}
+		if test.Txts != "" {
+			v.Set("txt", test.Txts)
+		}
+		URL := getFormUpdateURL(server.S.URL)
+
+		resp, err := http.PostForm(URL.String(), v)
+		if err != nil {
+			t.Log(server.Log.String())
+			t.Fatal("Failed to update URL:", URL.String(), err)
+		}
+		if resp.StatusCode != 200 {
+			t.Log(server.Log.String())
+			t.Errorf("FormNew: Wrong return Code: Got %d, expected %d. \n\tTest: %v",
+				resp.StatusCode,
+				200,
+				test)
+		}
+		doc, err := html.Parse(resp.Body)
+		if err != nil {
+			t.Fatal("Error parsing response Bopy")
+		}
+		errMsgs := checkForAlerts(doc)
+		if test.ErrCount != len(errMsgs) {
+			t.Errorf("Should have %d alert-warnings found: %d \nErrors: &v", test.ErrCount, len(errMsgs), errMsgs)
+		}
+
+		// Post Db check if user was actually created
+		if test.ErrCount == 0 {
+			// Check entry
+			e := server.Db.GetEntry(test.Entry.Hostname)
+			if e == nil {
+				t.Error("Domain does not match exist", test, e)
+			}
+			if e.String() != test.Entry.String() {
+				t.Errorf("Entries do not match: \nIs:\t %v\nEx:\t %v",
+					e,
+					&test.Entry)
+			}
+		}
+		if t.Failed() {
+			t.Logf("TEST: %v\n", test)
+			t.Log("\tErrMsgs:", errMsgs)
+			t.Log("\t", resp)
+			t.Log(server.Log.String())
+		}
+	}
 }
