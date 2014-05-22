4049971a15e74c87cebf1d3beeb85eebe84f0790
Matti <matti@moell.org>
2014-05-22 20:02:43 +0200

    database: Added String method to Entry

--- cooldns/database.go
+++ cooldns/database.go
@@ -1,6 +1,7 @@
 package cooldns
 
 import (
+	"fmt"
 	"net"
 )
 
@@ -17,6 +18,11 @@ type Entry struct {
 	Txts     []string
 	Mxs      []MxEntry
 	Cname    string
+}
+
+func (e *Entry) String() string {
+	return fmt.Sprintf("%s\n\tIpv6: %v\n\tIpv4: %v\n\tOffline: %v\n\tTxt: %v\n\tMxs: %v\n\tCname: %s",
+		e.Hostname, e.Ip6s, e.Ip4s, e.Offline, e.Txts, e.Mxs, e.Cname)
 }
 
 // Specifies the four methods that are needed from a DB
